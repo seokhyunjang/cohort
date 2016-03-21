c87e50a1217b9770779793fa8783531c34309665
seokhyunjang <savetheearth1112@gmail.com>
2016-03-21 11:35:16 +0900

    update cohort.sql: add cohort_size

--- cohort.sql
+++ cohort.sql
@@ -1,3 +1,5 @@
+응용문제 1 - 3월21일
+
 1) 유저를 첫번째 렌탈일 기준으로 나눈다(cohort)
 
 
@@ -8,20 +10,37 @@ DROP TEMPORARY TABLE IF EXISTS first_rental ;
 CREATE TEMPORARY TABLE first_rental
 
 SELECT
-r.customer_id ,
-MIN(r .rental_date) first_time
+    r.customer_id ,
+    MIN(r .rental_date) first_time
 
 FROM
-rental r
+    rental r
 
 GROUP BY 1
 ;
 
 SELECT *
 FROM
-first_rental
+    first_rental
 ;
 
 [2단계]
+각각의 cohort에 포함된 유저 수를 구한다(cohort size)
 
+# 2단계
+# 각각의 cohort에 포함된 유저 수를 구한다(cohort size)
+# Cohort Size
+# RPU를 구하기 위해 코호트 사이즈를 구하는 것이다.
 
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+
+SELECT
+	LEFT(first_time, 7) Month,
+	COUNT(*) num
+	
+FROM
+	first_rental
+	
+GROUP BY 1
+;
